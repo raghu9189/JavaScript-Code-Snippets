c247c3ae0d58a47b1e2360a66cbba1db7c2476d1
raghu9189 <raghu.ballu@gmail.com>
2025-07-05 18:19:59 +0530

    Updated Math Operations

--- mathOperations.js
+++ mathOperations.js
@@ -3,10 +3,10 @@ console.log(maxValue.toFixed(2));
 console.log(maxValue.valueOf());
 console.log(Math.max(1,2,3,4,5,6));
 console.log(Math.min(1,2,3,4,5,6));
-
+console.log(maxValue.toString())
 const myDate = new Date()
 console.log(myDate);
 
 const userInfo = "hello Raghu";
-const sanitizedinput = DOMP
+// const sanitizedinput = DOMP
 
