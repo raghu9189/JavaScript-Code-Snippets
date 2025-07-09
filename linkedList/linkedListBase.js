b209390bf4ceab29746c5bfb2d9f4531f082db4c
raghu9189 <raghu.ballu@gmail.com>
2025-07-09 21:06:36 +0530

    Updated linkedlist

---
+++ linkedList/linkedListBase.js
@@ -0,0 +1,14 @@
+// Node 
+class Node {
+    constructor(data, next = null){
+        this.data = data;
+        this.next = next;
+    }
+}
+
+// LinkedList
+class LinkedList{
+    constructor(){
+        
+    }
+}
