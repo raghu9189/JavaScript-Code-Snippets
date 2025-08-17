3e834d0890344c71f40870e0ec57a6cd81eaee61
raghu9189 <raghu.ballu@gmail.com>
2025-08-17 11:52:24 +0530

    add: linkedlist traverse

--- linkedList/linkedListBase.js
+++ linkedList/linkedListBase.js
@@ -6,9 +6,19 @@ class Node {
     }
 }
 
-// LinkedList
-class LinkedList{
-    constructor(){
-        
+function traverseList(head){
+
+    while(head != null){
+        console.log(head.data);
+        head = head.next
     }
 }
+
+function main(){
+    let head = new Node(10);
+    head.next = new Node(20);
+    head.next.next = new Node(30);
+    traverseList(head);
+}
+
+main()
