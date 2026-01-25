409194e7d8057f36242d7d4bd05154f03ac2d6b9
raghu9189 <raghu.ballu@gmail.com>
2026-01-25 13:14:09 +0530

    linkedlist

---
+++ linkedList/linked-list.js
@@ -0,0 +1,22 @@
+class Node{
+    constructor(data, next = null){
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList{
+    // Insert at Start method
+    insertAtStart(head, data){
+        this.data 
+    }
+
+}
+
+const list = new LinkedList();
+
+list.insertAtStart(20);
+list.insertAtStart(25);
+list.insertAtStart(30);
+
+
